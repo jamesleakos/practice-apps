038952343ccc47e49febbe1e7ead0064ffcea9a2
James Leakos <jamesleakos@gmail.com>
2022-11-23 19:01:03 -0500

    completed the bare min reqs

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,41 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/glossary');
 
-// 1. Use mongoose to establish a connection to MongoDB
-// 2. Set up any schema and models needed by the app
-// 3. Export the models
-// 4. Import the models into any modules that need them
+const termSchema = new mongoose.Schema({
+  term: {
+    type: String,
+    unique: true
+  },
+  definition: String
+})
+
+const Term = mongoose.model('Term', termSchema);
+
+const saveTerm = (_term, _definition) => {
+  const term = new Term();
+  term.term = _term;
+  term.definition = _definition;
+  return term.save();
+}
+
+const getTerms = () => {
+  return Term.find({});
+}
+
+const deleteTerm = (term) => {
+  return Term.deleteMany({term: term});
+}
+
+const modifyTerm = (term, newTerm, newDefinition) => {
+  return Term.updateMany(
+    { term: term },
+    {
+      $set: { term: newTerm, definition: newDefinition }
+    }
+  )
+}
+
+module.exports.saveTerm = saveTerm;
+module.exports.getTerms = getTerms;
+module.exports.deleteTerm = deleteTerm;
+module.exports.modifyTerm = modifyTerm;
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,71 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const db = require('./db.js');
 
 const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+app.get('/terms', (req, res) => {
+  db.getTerms()
+    .then(terms => {
+      res.status(200).send(terms);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).send('failed to get terms');
+    })
+});
+
+app.post('/terms', (req, res) => {
+  db.saveTerm(req.body.term, req.body.description)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).send('did not save');
+    })
+});
+
+app.put('/terms/:term', (req, res) => {
+  db.modifyTerm(req.params.term, req.body.newTerm, req.body.newDefinition)
+    .catch(err => {
+      console.log(err);
+      throw new Error('Did not change')
+    })
+    .then(() => {
+      return db.getTerms()
+    })
+    .then((terms) => {
+      res.status(200).send(terms);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    })
+});
+
+app.delete('/terms/:term', (req, res) => {
+  db.deleteTerm(req.params.term)
+    .catch(err => {
+      console.log(err);
+      throw new Error('Did not delete')
+    })
+    .then(() => {
+      return db.getTerms()
+    })
+    .then((terms) => {
+      res.status(200).send(terms);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    })
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
